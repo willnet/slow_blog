ebeb5a11fd3f7bf27108d7eb647dd128e571cecc
Shinichi Maeshima <netwillnet@gmail.com>
2024-10-09 18:13:21 +0900

    とりあえずseed足してユーザを作成した

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,7 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+if User.count.zero?
+  User.create!(email_address: "willnet@example.com", password: "password")
+end
