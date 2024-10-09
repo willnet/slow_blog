e1f34c9e8a9d97b8ccd7bd5dba187c5a742b749c
Shinichi Maeshima <netwillnet@gmail.com>
2024-10-09 18:13:09 +0900

    rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_10_09_085840) do
+ActiveRecord::Schema[8.0].define(version: 2024_10_09_091017) do
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "sessions", force: :cascade do |t|
     t.integer "user_id", null: false
     t.string "ip_address"
