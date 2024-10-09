b5cb2497afa4eb74f1ed61c3d761b621040a5048
Shinichi Maeshima <netwillnet@gmail.com>
2024-10-09 18:46:02 +0900

    userとpostの関連付けをした

--- db/migrate/20241009091017_create_posts.rb
+++ db/migrate/20241009091017_create_posts.rb
@@ -3,7 +3,7 @@ class CreatePosts < ActiveRecord::Migration[8.0]
     create_table :posts do |t|
       t.string :title
       t.text :body
-
+      t.references :user, foreign_key: true
       t.timestamps
     end
   end
